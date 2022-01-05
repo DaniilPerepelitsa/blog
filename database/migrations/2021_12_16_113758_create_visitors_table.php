0bc6f804b94c9a3f33d90cdb7d10ca5e65cc4431
fullistack <stackfull658@gmail.com>
2022-01-05 17:09:57 +0200

    added migration

---
+++ database/migrations/2021_12_16_113758_create_visitors_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVisitorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('visitors', function (Blueprint $table) {
+            $table->id();
+            $table->string('ip');
+            $table->unsignedBigInteger('user_id');
+            $table->date('visit_time');
+            $table->unsignedBigInteger('hits');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('visitors');
+    }
+}
